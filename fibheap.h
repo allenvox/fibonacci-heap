b5c1f987e45f5053b0f3829ccb994e9ac46c689b
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-11-15 13:33:02 +0700

    Edit header file

--- fibheap.h
+++ fibheap.h
@@ -18,18 +18,18 @@ typedef struct
 } fibheap;
 
 // - undone
+fibheap *fibheap_allocate_memory();
 fibheap *fibheap_insert(fibheap *heap, int key, char *value);
 void fibheap_add_node_to_root_list(node *n, node *min);
 node *fibheap_min(fibheap *heap);
-fibheap *fibheap_union(fibheap *heap1, fibheap *heap2); //
-void fibheap_link_lists(node* m1, node* m2);
-fibheap *fibheap_delete_min(fibheap *heap); //
+fibheap *fibheap_union(fibheap *heap1, fibheap *heap2);
+void fibheap_link_lists(node *m1, node *m2);
+fibheap *fibheap_delete_min(fibheap *heap);
 fibheap *fibheap_decrease_key(fibheap *heap, node *node, int new_key);
-fibheap *fibheap_delete(fibheap *heap, int key); //
-void fibheap_remove_node_from_root_list(node *n); //
+fibheap *fibheap_delete(fibheap *heap, node *x);
+void fibheap_remove_node_from_root_list(node *x, node *y);
 fibheap *fibheap_consolidate(fibheap *heap); //
-void fibheap_link(node* n, node* child);
-int fibheap_max_degree(fibheap* heap);
-void fibheap_cut(fibheap* heap, node* node_c, node* n); //
-void fibheap_cascading_cut(fibheap* heap, node* n); //
-void print(fibheap *heap); //
+void fibheap_link(fibheap *heap, node *y, node *x);
+void fibheap_cut(fibheap *heap, node *x, node* y);
+void fibheap_cascading_cut(fibheap *heap, node *n);
+int D(fibheap *heap);
