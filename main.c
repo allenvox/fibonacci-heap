41ffd1d03ce005229387a54c77ccc3dd1ebaf30e
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-11-15 13:37:40 +0700

    Implement a test program for fibheap

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include "fibheap.h"
 
 int main() {
+    fibheap *h1 = fibheap_allocate_memory();
+    h1 = fibheap_insert(h1, 5, "malfoy");
+    h1 = fibheap_insert(h1, 10, "harry");
+    h1 = fibheap_insert(h1, 15, "dobby");
+    h1 = fibheap_insert(h1, 20, "ron");
+    h1 = fibheap_insert(h1, 25, "hermione");
 
+    fibheap *h2 = fibheap_allocate_memory();
+    h2 = fibheap_insert(h2, 3, "dumbledore");
+    h2 = fibheap_insert(h2, 7, "snape");
+    h2 = fibheap_insert(h2, 9, "macgonagoll");
+    h2 = fibheap_insert(h2, 11, "hagrid");
+    h2 = fibheap_insert(h2, 13, "umbridge");
+
+    fibheap *h3 = fibheap_union(h1, h2);
     return 0;
 }
