3e0e53512a05fe106fc2444dc49e264eff6f936d
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-11-18 19:52:11 +0700

    Add fibheap printing out to test program

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ int main() {
     h1 = fibheap_insert(h1, 15, "dobby");
     h1 = fibheap_insert(h1, 20, "ron");
     h1 = fibheap_insert(h1, 25, "hermione");
+    fibheap_print(h1);
 
     fibheap *h2 = fibheap_allocate_memory();
     h2 = fibheap_insert(h2, 3, "dumbledore");
@@ -14,7 +15,9 @@ int main() {
     h2 = fibheap_insert(h2, 9, "macgonagoll");
     h2 = fibheap_insert(h2, 11, "hagrid");
     h2 = fibheap_insert(h2, 13, "umbridge");
+    fibheap_print(h2);
 
     fibheap *h3 = fibheap_union(h1, h2);
+    fibheap_print(h3);
     return 0;
 }
