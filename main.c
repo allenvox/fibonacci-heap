3780af933ebbca96dfa43868faea5a8facfa2f57
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-11-21 17:38:26 +0700

    Modify test program

--- main.c
+++ main.c
@@ -8,7 +8,9 @@ int main() {
     h1 = fibheap_insert(h1, 15, "dobby");
     h1 = fibheap_insert(h1, 20, "ron");
     h1 = fibheap_insert(h1, 25, "hermione");
+    printf("Heap 1:\n");
     fibheap_print(h1);
+    printf("\n");
 
     fibheap *h2 = fibheap_allocate_memory();
     h2 = fibheap_insert(h2, 3, "dumbledore");
@@ -16,15 +18,19 @@ int main() {
     h2 = fibheap_insert(h2, 9, "macgonagoll");
     h2 = fibheap_insert(h2, 11, "hagrid");
     h2 = fibheap_insert(h2, 13, "umbridge");
+    printf("Heap 2:\n");
     fibheap_print(h2);
+    printf("\n");
 
     fibheap *h3 = fibheap_union(h1, h2);
+    printf("Heap 3:\n");
     fibheap_print(h3);
+    printf("\n");
 
     node *min = fibheap_min(h3);
     printf("Value of minimal: '%s'\n", min->value);
     fibheap_delete_min(h3);
     min = fibheap_min(h3);
-    printf("New minimal is '%s'\n", min->value);
+    printf("New minimal: '%s'\n", min->value);
     return 0;
 }
